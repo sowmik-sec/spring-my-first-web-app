fbad70f8372c050223489b1946fe3111c51c9e3f
sowmik-sec <sowmik.sec@gmail.com>
2023-11-17 12:07:41 +0600

    quick overview importance of logging with spring boot

--- src/main/java/com/sowmik/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/sowmik/myfirstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.sowmik.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+//    private Logger logger = LoggerFactory.getLogger(getClass());
     @RequestMapping("login")
     public String gotoLoginPage(@RequestParam String name, ModelMap model) {
         model.put("name", name);
-        System.out.println("Request param is "+name); // NOT RECOMMENDED FOR PRODUCTION CODE
+//        logger.debug("Request param is {}",name);
+//        logger.info("I want this printed at info level, {}",name);
+//        logger.warn("I want this printed at warn level, {}",name);
+//        System.out.println("Request param is "+name); // NOT RECOMMENDED FOR PRODUCTION CODE
         return "login";
     }
 }
