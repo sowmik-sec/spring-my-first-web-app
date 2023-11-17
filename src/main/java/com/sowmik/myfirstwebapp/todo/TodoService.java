388ca7242f332a1f74e0211f79fdf93454644a0e
sowmik-sec <sowmik.sec@gmail.com>
2023-11-17 20:04:58 +0600

    create first version of list todos page

--- src/main/java/com/sowmik/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/sowmik/myfirstwebapp/todo/TodoService.java
@@ -1,10 +1,14 @@
 package com.sowmik.myfirstwebapp.todo;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class TodoService {
-    private static List<Todo> todos;
+    private static List<Todo> todos = new ArrayList<>();
     static {
         todos.add(new Todo(1, "sowmik", "Learn AWS", LocalDate.now().plusYears(1), false));
         todos.add(new Todo(2, "sowmik", "Learn DevOps", LocalDate.now().plusYears(1), false));
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>List Todos Page</title>
+</head>
+<body>
+    <div>Welcome to com.sowmik</div>
+    <div>Your Todos are ${todos}</div>
+<%--    <div>Your Password: ${password}</div>--%>
+</body>
+</html>
