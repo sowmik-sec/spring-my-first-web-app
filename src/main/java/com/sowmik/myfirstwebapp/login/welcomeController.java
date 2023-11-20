7ba5b6d76e010ea5acb99e3cb0a1b15f018e5ca0
sowmik-sec <sowmik.sec@gmail.com>
2023-11-20 10:14:50 +0600

    refactor and remove hardcoding of user id

--- src/main/java/com/sowmik/myfirstwebapp/login/welcomeController.java
+++ src/main/java/com/sowmik/myfirstwebapp/login/welcomeController.java
@@ -1,5 +1,7 @@
 package com.sowmik.myfirstwebapp.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,12 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 public class welcomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String gotoWelcomePage(ModelMap model) {
-        model.put("name", "Sowmik");
+        model.put("name", getLoggedInUsername());
         return "welcome";
+    }
+
+    private String getLoggedInUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
     }
 }
--- src/main/java/com/sowmik/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/sowmik/myfirstwebapp/todo/TodoController.java
@@ -1,6 +1,8 @@
 package com.sowmik.myfirstwebapp.todo;
 
 import jakarta.validation.Valid;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -24,14 +26,20 @@ public class TodoController {
 
     @RequestMapping(value = "list-todos")
     public String listAllTodos(ModelMap model) {
-        List<Todo> todos = todoService.findByUsername("sowmik");
+        String username = getLoggedInUsername(model);
+        List<Todo> todos = todoService.findByUsername(username);
         model.addAttribute("todos", todos);
         return "listTodos";
     }
 
+    private static String getLoggedInUsername(ModelMap model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
+    }
+
     @RequestMapping(value = "add-todo", method = RequestMethod.GET)
     public String showNewTodoPage(ModelMap model) {
-        String username = (String) model.get("name");
+        String username = getLoggedInUsername(model);
         Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1),false);
         model.put("todo", todo);
         return "todo";
@@ -42,7 +50,7 @@ public class TodoController {
         if(result.hasErrors()) {
             return "todo";
         }
-        String username = (String) model.get("name");
+        String username = getLoggedInUsername(model);
         todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), false);
         return "redirect:list-todos";
     }
@@ -65,7 +73,7 @@ public class TodoController {
         if(result.hasErrors()) {
             return "todo";
         }
-        String username = (String) model.get("name");
+        String username = getLoggedInUsername(model);
         todo.setUsername(username);
         todoService.updateTodo(todo);
         return "redirect:list-todos";
