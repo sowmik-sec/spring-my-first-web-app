7745d601e3ae528deb5d1a743669c5de32c7a411
sowmik-sec <sowmik.sec@gmail.com>
2023-11-20 09:57:59 +0600

    configuring spring security with custom user and password encoder

--- src/main/java/com/sowmik/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/sowmik/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -4,17 +4,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.function.Function;
 
 @Configuration
 public class SpringSecurityConfiguration {
     // LDAP or Database
     // In Memory
+    Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
-        UserDetails userDetails = User.withDefaultPasswordEncoder().username("sowmik").password("dummy").roles("USER", "ADMIN").build();
+        UserDetails userDetails = User.builder().passwordEncoder(passwordEncoder).username("sowmik").password("dummy").roles("USER", "ADMIN").build();
         return new InMemoryUserDetailsManager(userDetails);
     }
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
