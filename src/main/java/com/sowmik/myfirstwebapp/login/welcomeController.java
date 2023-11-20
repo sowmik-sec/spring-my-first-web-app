d4f6a54cf253b47b461d2d71c8c9207008d9310f
sowmik-sec <sowmik.sec@gmail.com>
2023-11-20 09:33:20 +0600

    preparing for spring security

---
+++ src/main/java/com/sowmik/myfirstwebapp/login/welcomeController.java
@@ -0,0 +1,17 @@
+package com.sowmik.myfirstwebapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name")
+public class welcomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String gotoWelcomePage(ModelMap model) {
+        model.put("name", "Sowmik");
+        return "welcome";
+    }
+}
