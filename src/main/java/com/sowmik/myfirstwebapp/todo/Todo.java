028e51e1cb82141a72f74e1915cc8291b254b3cd
sowmik-sec <sowmik.sec@gmail.com>
2023-11-21 20:41:04 +0600

    make todo an entity and populate todo data into h2

--- src/main/java/com/sowmik/myfirstwebapp/todo/Todo.java
+++ src/main/java/com/sowmik/myfirstwebapp/todo/Todo.java
@@ -1,5 +1,9 @@
 package com.sowmik.myfirstwebapp.todo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
@@ -7,13 +11,24 @@ import java.time.LocalDate;
 //Database (MySQL)
 //Static List of todos => Database (H2, MySQL)
 
+//JPA
+//Bean -> Database
+
+@Entity
 public class Todo {
+
+    @Id
+    @GeneratedValue
     private int id;
     private String username;
     @Size(min = 10, message = "Enter at least 10 characters")
     private String description;
     private LocalDate targetDate;
     private boolean done;
+
+    public Todo() {
+
+    }
 
     public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
         this.id = id;
